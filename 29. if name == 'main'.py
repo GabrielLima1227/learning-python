caa6ad127789903e744ffa96da3c672b19d1bda9
Gabriel de Lima Sousa <gablimax@hotmail.com>
2025-05-22 09:16:17 -0300

    feat: Ajuste no Histórico

---
+++ 29. if name == 'main'.py
@@ -0,0 +1,3 @@
+# if __name__ == "__main__":
+# Este script pode ser importado (como um módulo) OU executado diretamente (como um programa autônomo).
+# Funções e classes neste módulo podem ser reutilizadas sem que o bloco principal de código seja executado.
